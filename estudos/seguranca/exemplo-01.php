cece7c007f025953b5e464a234db31f60669ecf4
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2019-04-17 23:06:16 -0300

    pasta psrs e seguranca

---
+++ estudos/seguranca/exemplo-01.php
@@ -0,0 +1,24 @@
+<?php  
+
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+	#defesa
+	$cmd = escapeshellcmd($_POST["cmd"]); 
+
+	var_dump($cmd); 
+
+	echo "<pre>"; 
+
+	$comando = system($cmd,$retorno); 
+
+	echo "</pre>"; 
+
+}
+
+?>
+
+<form method="POST">
+	<input type="text" name="cmd">
+	<button type="submit">Enviar</button>
+</form>
